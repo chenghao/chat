13fe19f94eed8b06677a96558f4f7b442f4969dd
chenghao <hao2432@gmail.com>
2014-07-04 16:48:46 +0800

    新增支持sqlite3

--- src/dbutil.py
+++ src/dbutil.py
@@ -3,15 +3,23 @@
 import torndb
 from hashlib import md5
 
+import sqlite3
+import models
+
 DBHOST = "localhost"
 DBPORT = 3306
 DBUSER = "root"
-#DBPWD = "0123456789"
-DBPWD = "hao"
+DBPWD = "0123456789"
+#DBPWD = "hao"
 DBNAME = "chat"
 
+"""
 conn = torndb.Connection("%s:%s" % (DBHOST, str(DBPORT)), DBNAME, DBUSER, DBPWD)
 cursor = conn._cursor()
+"""
+conn = sqlite3.connect("chat.db")
+conn.text_factory = str
+cursor = conn.cursor()
 
 
 def authorized(url='/login'):
@@ -21,10 +29,10 @@ def authorized(url='/login'):
             user_login_name_cookie = self.get_cookie('loginName', '')
             user_password_cookie = self.get_cookie('password', '')
             if user_login_name_cookie and user_password_cookie:
-                #from dbutil import User
                 user = User.check_user(user_login_name_cookie, user_password_cookie)
             else:
                 user = False
+
             if request.method == 'GET':
                 if not user:
                     self.redirect(url)
@@ -41,6 +49,18 @@ def authorized(url='/login'):
 
 
 class User():
+
+    def __init__(self, conn):
+        create_table_sql = """
+          CREATE TABLE IF NOT EXISTS `user` (
+            `loginName` TEXT NOT NULL ,
+            `password` TEXT NOT NULL ,
+            `nickName` TEXT NOT NULL ,
+            `serial` TEXT NOT NULL ,
+            `status` INTEGER DEFAULT '1'
+          ) """
+        conn.execute(create_table_sql)
+
     def check_user(self, loginName='', password=''):
         if loginName and password:
             user = self.get_user_by_name(loginName)
@@ -52,25 +72,46 @@ class User():
             return False
 
     def get_user_by_name(self, loginName):
+        """
         conn._ensure_connected()
         return conn.get('SELECT * FROM `user` WHERE `loginName` = \'%s\' LIMIT 1' % str(loginName))
+        """
+        user = conn.execute('SELECT * FROM `user` WHERE `loginName` = \'%s\' LIMIT 1' % str(loginName)).fetchone()
+        if user:
+            user1 = models.User(user[0], user[1], user[2], user[3], user[4])
+            return user1
+        else:
+            return None
+
 
     def add_user(self, loginName, password, nickName, serial):
         if loginName and password and nickName and serial:
-            sql = "insert into `user` (`loginName`, `password`, `nickName`, `serial`) values(%s, %s, %s, %s)"
+            '''
+            sql = "insert into user (loginName, password, nickName, serial) values(%s, %s, %s, %s)"
             conn._ensure_connected()
             return conn.execute(sql, loginName, md5(password.encode('utf-8')).hexdigest(), nickName, serial)
+            '''
+            sql = "insert into user (loginName, password, nickName, serial) values(?, ?, ?, ?)"
+            c = conn.execute(sql, (loginName.encode('utf-8'), md5(password.encode('utf-8')).hexdigest(), nickName.encode('utf-8'), str(serial)))
+            conn.commit()
+            return c.rowcount
         else:
             return None
 
     def update_user_status(self, loginName, status):
         if loginName and status:
+            '''
             sql = "update user set status = %s where loginName = %s"
             conn._ensure_connected()
             return conn.execute(sql, status, loginName)
+            '''
+            sql = "update user set status = ? where loginName = ?"
+            c = conn.execute(sql, (status, loginName))
+            conn.commit()
+            return c.rowcount
         else:
             return None
 
-User = User()
-
+#User = User()
+User = User(conn)
 
--- src/models.py
+++ src/models.py
@@ -1,11 +1,9 @@
 #coding:utf-8
 
 class User():
-    def __init__(self,pid,loginName,password,nickName,serial):
-        
-        self.pid = pid
+    def __init__(self, loginName, password, nickName, serial, status):
         self.loginName = loginName
         self.password = password
         self.nickName = nickName
         self.serial = serial
-
+        self.status = status
